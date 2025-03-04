b8ef1e783861df46df2efb7ca15d0570befe5415
Andrei Konovalov <a.konovaloff13@yandex.com>
2025-03-04 12:37:20 +0400

    The test data is moved out from the test class

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,20 +1,20 @@
-import pytest
 from praktikum.bun import Bun
+
+test_bun = {"name": "Super bun", "price": 15}
 
 
 class TestBun:
-    test_bun = {"bun_name": "Super bun", "bun_price": 15}
 
     def test_get_name(self):
-        tested_name = self.test_bun["bun_name"]
+        tested_name = test_bun["name"]
         bun = Bun(tested_name,
-                  self.test_bun["bun_price"])
+                  test_bun["price"])
 
         assert bun.get_name() == tested_name, "Булочка должна иметь заданное имя"
 
     def test_get_price(self):
-        tested_price = self.test_bun["bun_price"]
-        bun = Bun(self.test_bun["bun_name"],
-                  self.test_bun["bun_price"])
+        tested_price = test_bun["price"]
+        bun = Bun(test_bun["name"],
+                  test_bun["price"])
 
         assert bun.get_price() == tested_price, "Булочка должна иметь заданную цену"
